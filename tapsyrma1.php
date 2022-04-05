0ca3690a01d6344c6cfd2e57d1ac0e7652de4c5f
Mukhtarova <99592206+Mukhtarova@users.noreply.github.com>
2022-04-05 21:28:35 +0600

    Add files via upload

---
+++ tapsyrma1.php
@@ -0,0 +1,8 @@
+<?php
+	$a=8;
+	$b=13;
+	if (($a>=2 && $a<12) || ($b>6 && $b<14))
+		echo"true \r\n";
+	else
+		echo"false";
+?>
---
+++ tapsyrma2.php
@@ -0,0 +1,44 @@
+<?php
+	$a=7;
+	switch($a){
+    case 1:
+        echo "Қыс";
+    break;
+    case 2:
+        echo "Қыс";
+    break;
+    case 3:
+        echo "Көктем";
+    break;
+    case 4:
+        echo "Көктем";
+    break;
+    case 5:
+       echo "Көктем";
+    break;
+    case 6:
+        echo "Жаз";
+    break;
+    case 7:
+        echo "Жаз";
+    break;
+    case 8:
+        echo "Жаз";
+    break;
+    case 9:
+        echo "Күз";
+    break;
+    case 10:
+        echo "Күз";
+    break;
+    case 11:
+        echo "Күз";
+    break;
+    case 12:
+        echo "Қыс";
+    break;
+    default:
+       echo "Тек 1-12 сандарың еңгізіңіз";
+    break;
+}
+?>
